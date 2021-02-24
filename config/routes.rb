39046705a34e0d91cd6b480e67ea35801b420a4b
brian <riceb53@gmail.com>
2021-02-23 21:42:52 -0500

    make seeds file, add all products page and two individual products pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/products" => "products#index"
+    get "/lamp" => "products#lamp"
+    get "/sleeping_bag" => "products#sleeping_bag"
+  end
 end
