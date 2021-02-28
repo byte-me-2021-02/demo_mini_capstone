24b9fced85acffd1164512738c1b7bd5735b07d0
brian <riceb53@gmail.com>
2021-02-28 11:29:56 -0500

    add products create action

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,4 +3,18 @@ class Api::ProductsController < ApplicationController
     @products = Product.all
     render 'index.json.jb'
   end
+
+  def create
+    @product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description]
+    )
+    @product.save
+    # take in some parameters
+    # save that data to the db
+    # show the user the newly made product
+    render 'show.json.jb'
+  end
 end
