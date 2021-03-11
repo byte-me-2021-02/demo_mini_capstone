04b0b27d6cd91c499bab9b2a7df08d1fc5e30fc0
brian <riceb53@gmail.com>
2021-03-10 19:47:07 -0500

    finish orders create action

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,14 +1,25 @@
 class Api::OrdersController < ApplicationController
   def create
+    p "current_user"
+    p current_user
+    p "current_user"
+
+    # subtotal == quantity * price
+    product = Product.find_by(id: params[:product_id])
+    calculated_subtotal = params[:quantity].to_i * product.price # 300
+    calculated_tax = calculated_subtotal * 0.09
+    calculated_total = calculated_subtotal + calculated_tax
+    
     @order = Order.new(
-      product_id: 17,
-      user_id: 1,
-      quantity: 10,
-      tax: 10.80,
-      subtotal: 120,
-      total: 130.80
+      product_id: params[:product_id],
+      user_id: current_user.id,
+      quantity: params[:quantity],
+      tax: calculated_tax,
+      subtotal: calculated_subtotal,
+      total: calculated_total
     )
-    @order.save
+    
+    @order.save!
     render 'show.json.jb'
   end
 end
